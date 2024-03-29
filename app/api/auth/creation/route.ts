96486e25f14515e248b7f49669d4b1385652d0e4
Damon Redding <reddingdamon0@gmail.com>
2024-03-29 16:04:13 -0400

    Add kinde-auth-nextjs server-side authentication to GET route

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
   const user = await getUser();
 
   if (!user || user === null || !user.id) {
-    throw new Error("Smoething went wrong, i am srorry....");
+    throw new Error("Something went wrong, i am sorry....");
   }
 
   let dbUser = await prisma.user.findUnique({
